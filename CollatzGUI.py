7d22d28f5a294d025ce75ddf8502ac1add7c4c3d
SidneiMe <sidh_end@hotmail.com>
2023-05-30 19:04:45 -0300

    Inclusão do projeto

---
+++ CollatzGUI.py
@@ -0,0 +1,214 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Função para plotar o gráfico com base nos valores do vetor
+def plot_grafico():
+    vetor = np.array(list_1)
+
+    fig, ax = plt.subplots(figsize=(6.04, 4.84), dpi=100)
+    fig.patch.set_facecolor("black")
+
+    # Configurações do gráfico
+    ax.plot(range(1, len(vetor) + 1), vetor, color="lime", linewidth=2)
+    ax.set_xlabel("Index", color="white")
+    ax.set_ylabel("Value", color="white")
+    ax.set_title("Intermediate Values", color="white", fontsize=16)
+    ax.set_facecolor("black")
+
+    # Configuração dos eixos
+    ax.spines["bottom"].set_color("lime")
+    ax.spines["left"].set_color("lime")
+    ax.spines["top"].set_color("lime")
+    ax.spines["right"].set_color("lime")
+    ax.tick_params(colors="lime", labelsize=10)
+    ax.title.set_color("lime")
+    ax.xaxis.label.set_color("lime")
+    ax.yaxis.label.set_color("lime")
+    ax.tick_params(axis="both", which="both", top=False, right=False)
+
+    # Configurações da grade do gráfico
+    ax.grid(True, color="lime", linestyle="dashed", linewidth=0.5)
+
+    # Salva o gráfico em uma imagem temporária
+    temp_filename = "temp_plot.png"
+    plt.savefig(temp_filename, facecolor="black", bbox_inches="tight", dpi=100)
+
+    # Abre a imagem e exibe na interface
+    image = Image.open(temp_filename)
+    photo = ImageTk.PhotoImage(image)
+    label_imagem.configure(image=photo)
+    label_imagem.image = photo
+
+
+# Função para calcular a sequência de Collatz com base no número digitado pelo usuário
+def Collatz():
+    counter = 0
+    orbital = 0
+
+    # Limpa a lista, atualiza a interface e obtém o número digitado
+    list.delete(0, END)
+    list_1.clear()
+    root.update()
+    c0 = int(text_c0.get())
+
+    # Adiciona o número inicial à lista
+    list_1.append(c0)
+
+    while c0 != 1:
+        # Insere o número atual na lista
+
+        list.insert(counter, c0)
+        # Se o número atual é par, divide por 2
+        if c0 % 2 == 0:
+            c0 /= 2
+            counter += 1
+            list_1.append(c0)
+
+        else:
+            # Se o número atual é impar, multiplica por 3 e adiciona 1
+            c0 = 3 * c0 + 1
+            counter += 1
+            list_1.append(c0)
+
+        # Atualiza o número orbital (maior encontrado até agora)
+        if orbital < c0:
+            orbital = c0
+
+    # Insere número final, o número de passos e o maior número na lista
+    string_number = str(counter)
+    string_number2 = str(orbital)
+    list.insert(END, c0)
+    list.insert(END, "Steps: " + string_number)
+    list.insert(END, "The highest number: " + string_number2)
+
+    # Chama a função para plotar o gráfico
+    plot_grafico()
+
+    # Faz a lista rolar até o final e atualiza a barra de rolagem
+    list.see(END)
+    scrollbar.set(*list.yview())
+
+
+# Lista vazia para armazenar os valores da sequência de Collatz
+list_1 = []
+
+# Definição da cor lime para ser utilizada em vários elementos da interface
+lime = "#00ff00"
+
+root = Tk()
+
+# Dimensões da janela
+width = 1280
+height = 620
+
+# Centraliza a janela na tela
+largura_screen = root.winfo_screenwidth()
+altura_screen = root.winfo_screenheight()
+posx = largura_screen / 2 - width / 2
+posy = altura_screen / 2 - height / 2
+root.geometry("%dx%d+%d+%d" % (width, height, posx, posy))
+
+# Define o tamanho mínimo e máximo da janela
+root.minsize(1280, 620)
+root.maxsize(1280, 620)
+
+# Define a cor de fundo da janela como preto
+root["bg"] = "black"
+
+# Define o título da janela
+root.title("A Hipótese de Collatz")
+
+# Define o ícone da janela
+root.iconbitmap("icons/pythonlogo.ico")
+
+# Cria uma instância do estilo padrão do ttk para personalizar os elementos da interface
+style = ttk.Style()
+style.theme_use("default")
+
+# Configurações da barra de rolagem
+style.configure(
+    "TScrollbar",
+    background=lime,
+    troughcolor="black",
+    gripcount=0,
+    darkcolor=lime,
+    lightcolor=lime,
+)
+
+# Rótulo principal
+label_1 = Label(
+    root,
+    text="The Collatz Conjecture",
+    font="Times 20 bold italic",
+    bg="black",
+    fg=lime,
+    anchor=CENTER,
+    justify=CENTER,
+)
+
+# Rótulo para instrução
+label_2 = Label(
+    root,
+    text="Type a number:",
+    font="Times 13 bold",
+    bg="black",
+    fg=lime,
+    anchor=CENTER,
+    justify=CENTER,
+)
+
+# Ícone do Python
+img = PhotoImage(file="images/rsz_snake.png")
+
+# Rótulo para exibir a imagem do ícone
+label_imagem = Label(
+    root,
+    image=img,
+    background="black",
+)
+
+# Barra de rolagem vertical
+scrollbar = ttk.Scrollbar(root, style="TScrollbar")
+
+# Lista para exibir os valores da sequência de Collatz
+list = Listbox(
+    root,
+    width=100,
+    height=30,
+    background="black",
+    fg="lime",
+    yscrollcommand=scrollbar.set,
+)
+
+# Campo de entrada para o número inicial
+text_c0 = Entry(
+    root, 
+    bg="lime", 
+    fg="black", 
+    width="100", 
+    text="Digite um número."
+)
+
+# Botão para iniciar o algoritmo de Collatz
+btn1 = Button(
+    root, 
+    text="Enviar", 
+    bg="black", 
+    fg="lime", 
+    command=Collatz
+)
+
+# Posicionamento dos elementos na interface
+label_1.grid(row=0, column=0)
+label_imagem.grid(row=1, column=1, padx=15)
+list.grid(row=1, column=0, sticky=NE, padx=(10, 0))
+scrollbar.grid(row=1, column=1, sticky="wns")
+scrollbar.config(command=list.yview)
+label_2.grid(row=2, column=0, sticky=SW, padx=(10, 0))
+text_c0.grid(row=3, column=0, sticky=W, padx=(10, 0))
+btn1.grid(row=4, column=0, sticky=E, pady=3)
+
+root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # CollatzGUI
- 
+
+## The Collatz Conjecture
+
+## Descrição
+O projeto "CollatzGUI" é uma implementação interativa e visual do famoso problema matemático conhecido como "A Hipótese de Collatz". Essa hipótese, também chamada de "Conjectura 3n+1", é uma sequência numérica que, independentemente do número inicial escolhido, eventualmente chega ao número 1.
+
+Com esta aplicação, você pode explorar e visualizar a sequência de Collatz para diferentes números iniciais, além de obter informações sobre o número de passos necessários para chegar ao número 1 e o maior número encontrado ao longo do processo.
+
+Através de uma interface gráfica, você pode inserir o número inicial desejado e iniciar o cálculo da sequência. Os valores da sequência são exibidos em uma lista na tela, enquanto um gráfico mostra a evolução dos números ao longo do tempo. O projeto oferece uma maneira interativa e envolvente de compreender e explorar essa intrigante conjectura matemática.
+
+## Recursos
+- Interface gráfica interativa para inserir o número inicial e visualizar os resultados.
+- Lista dos valores da sequência de Collatz exibidos em tempo real.
+- Gráfico dinâmico que representa a evolução dos números ao longo do tempo.
+- Informações sobre o número de passos necessários para chegar ao número 1.
+- Exibição do maior número encontrado durante o processo.
+- Implementação eficiente e rápida do algoritmo de Collatz.
+- Visualização intuitiva dos resultados.
+
+## Instalação
+Siga estas etapas para executar o projeto em seu ambiente local:
+
+1. Certifique-se de ter o Python instalado (versão 3.6 ou superior) em seu sistema.
+
+2. Clone este repositório para o seu computador:
+   ```shell
+   git clone https://github.com/seu-usuario/CollatzGUI.git
+Navegue até o diretório do projeto:
+
+shell
+Copy code
+cd CollatzGUI
+Instale as dependências usando o pip:
+
+shell
+Copy code
+pip install -r requirements.txt
+Execute o script do projeto:
+
+shell
+Copy code
+python collatz_gui.py
+A interface gráfica será aberta. Digite um número inicial e clique no botão "Enviar" para iniciar o cálculo da sequência de Collatz.
+
+Os valores da sequência serão exibidos na lista e um gráfico será gerado mostrando a evolução dos números.
+
+Para sair do aplicativo, feche a janela.
+
+Contribuição
+Contribuições são bem-vindas! Se você quiser melhorar este projeto, siga estas etapas:
+
+Faça um fork do repositório.
+
+Crie uma branch para suas alterações:
+
+shell
+Copy code
+git checkout -b minha-branch
+Faça as alterações desejadas e adicione os arquivos modificados:
+
+shell
+Copy code
+git add .
+Faça o commit das alterações:
+
+shell
+Copy code
+git commit -m "Minhas alterações"
+Faça push das alterações para a sua branch:
+
+shell
+Copy code
+git push origin minha-branch
+``
